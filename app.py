43cf67e2d714ca9bc2583398bf1ce5e02269f2a5
loaywaleed <loaywaleed9@gmail.com>
2024-05-12 17:02:47 +0300

    setting up socketio

--- app.py
+++ app.py
@@ -5,14 +5,28 @@ MRT Api
 
 from flask import Flask, jsonify
 from flask_socketio import SocketIO
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 app = Flask('__name__')
+app.config['SECRET_KEY'] = getenv("SECRET_KEY")
+
 
 socketio = SocketIO(app)
 
-@app.route('/')
+
+@app.route("/", strict_slashes=False, methods=["GET"])
 def home():
-    return jsonify({"name": "Menofia Racing Team"})
+    return "MRT"
+
+
+@app.route("/voltage", strict_slashes=False, methods=["GET"])
+def voltage():
+    socketio.emit('my_event', {'data': 'Some data'}, namespace='/s')
+    return jsonify({"data": "12 V"})
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
