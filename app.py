d3b2145bdb9adae2a4e7d6a84f84bbeabed58e48
loaywaleed <loaywaleed9@gmail.com>
2024-05-12 21:32:41 +0300

    allowing cors

--- app.py
+++ app.py
@@ -7,14 +7,22 @@ from flask import Flask, jsonify
 from flask_socketio import SocketIO
 from dotenv import load_dotenv
 from os import getenv
+from flask_cors import CORS
 
+
+# Flask Config
 load_dotenv()
 
 app = Flask('__name__')
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
+socketio = SocketIO(app, cors_allowed_origins="*")
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-socketio = SocketIO(app)
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+    socketio.emit('message', 'Hello from Flask')
 
 
 @app.route("/", strict_slashes=False, methods=["GET"])
@@ -22,10 +30,10 @@ def home():
     return "MRT"
 
 
-@app.route("/voltage", strict_slashes=False, methods=["GET"])
-def voltage():
-    socketio.emit('my_event', {'data': 'Some data'}, namespace='/s')
-    return jsonify({"data": "12 V"})
+# @app.route("/voltage", strict_slashes=False, methods=["GET"])
+# def voltage():
+#     socketio.emit('my_event', {'data': 'Some data'}, namespace='/s')
+#     return jsonify({"data": "12 V"})
 
 
 if __name__ == '__main__':
