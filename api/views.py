3f71dc58e7d96fa4c85b97d6d8af0ffffe23c369
loaywaleed <loaywaleed9@gmail.com>
2024-05-12 23:01:19 +0300

    post voltage route

--- api/views.py
+++ api/views.py
@@ -2,14 +2,20 @@
 app routes
 """
 
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, request
 
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/voltage", strict_slashes=False, methods=["GET"])
-def voltage():
-    # socketio.emit('voltage', {'data': '12 V'})
-    print("message sent")
+def get_voltage():
     return jsonify({"data": "12 V"})
+
+
+@views.route("/voltage", strict_slashes=False, methods=["POST"])
+def post_voltage():
+    # socketio.emit('voltage', {'data': '12 V'})
+    data = request.json
+    print("message sent")
+    return jsonify(data)
